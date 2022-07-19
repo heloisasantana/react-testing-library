d8366dce2f9481ceaa36407ee78a06224ba25997
heloisasantana <heloisasantana.silva@hotmail.com>
2022-07-19 15:55:15 -0300

    Conclusão do Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('4- Teste o componente NotFound.js', () => {
+  test('Teste se a página contém um heading h2 com o texto correto', () => {
+    render(<NotFound />);
+    const objInfo = { level: 2, name: /Page requested not found/i };
+    const pageNotFound = screen.getByRole('heading', objInfo);
+    expect(pageNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem correta', () => {
+    render(<NotFound />);
+    const alt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(alt);
+    const link = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img).toHaveAttribute('src', link);
+  });
+});
